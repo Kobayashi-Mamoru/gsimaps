37b1bab8ffd40fb6173ddbdbb6a0773ab1c1cf64
Kobayashi-Mamoru <37054518+Kobayashi-Mamoru@users.noreply.github.com>
2018-03-05 11:25:25 +0900

    Update gsimaps.js

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -1,4 +1,4 @@
-/************************************************************************
+,/************************************************************************
  設定
  ************************************************************************/
 
@@ -28,7 +28,8 @@ CONFIG.layersTab = [
 			'./layers_txt/layers_topic.txt',
 			'./layers_txt/layers_skhb.txt',
 			'./layers_txt/layers_topic_kusatsushirane.txt',
-			'./layers_txt/layers_topic_nishi.txt'
+			'./layers_txt/layers_topic_nishi.txt',
+			'./layers_txt/mamoru1.txt'
 					]
 	},
 	{
